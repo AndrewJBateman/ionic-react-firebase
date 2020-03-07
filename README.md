53b925faafd540981a4b9b5ff42f1b232d5006f9
Andrew Bateman <gomezbateman@yahoo.com>
2020-03-07 22:06:01 +0100

    Ion list added with info from Ionic api

---
+++ README.md
@@ -0,0 +1,59 @@
+# Angular React Firebase
+
+App using Ionic with React to store data in a Firebase backend.
+
+*** Note: to open web links in a new window use: _ctrl+click on link_**
+
+## Table of contents
+
+* [General info](#general-info)
+* [Screenshots](#screenshots)
+* [Technologies](#technologies)
+* [Setup](#setup)
+* [Features](#features)
+* [Status](#status)
+* [Inspiration](#inspiration)
+* [Contact](#contact)
+
+## General info
+
+* 
+
+## Screenshots
+
+![Example screenshot](./img/app.png)
+
+## Technologies
+
+* [Ionic/React v5.0.0](https://www.npmjs.com/package/@ionic/react)
+
+## Setup
+
+* Install dependencies using `npm i`
+* Run `ionic serve` to open the dev server at `http://localhost:8100/`
+
+## Code Examples
+
+* 
+
+```typescript
+
+```
+
+## Features
+
+* 
+
+## Status & To-Do List
+
+* Status: In progress - video 9 completed
+* To-Do: Complete series of videos
+
+## Inspiration
+
+* [CodeDamn Youtube Video Series: Ionic + React + Firebase #1:](https://www.youtube.com/watch?v=WugT638KS3Q)
+* [Ionic api list of people to use in app](https://ionicframework.com/docs/demos/api/list/)
+
+## Contact
+
+Repor created by [ABateman](https://www.andrewbateman.org) - feel free to contact me!
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,25 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonList, IonTitle, IonToolbar, IonItem, IonAvatar, IonLabel, IonItemSliding, IonItemOptions, IonItemOption } from '@ionic/react';
 import React from 'react';
 import './Home.css';
+
+const arr = [
+  {
+    name: 'Finn',
+    desc: 'great guy'
+  },
+  {
+    name: 'Han',
+    desc: 'Funny guy'
+  },
+  {
+    name: 'Rey',
+    desc: 'Superb guy'
+  },
+  {
+    name: 'Luke',
+    desc: 'Messy guy'
+  }
+]
 
 const Home: React.FC = () => {
   return (
@@ -10,7 +29,30 @@ const Home: React.FC = () => {
           <IonTitle>Hello World</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent className="ion-padding">Ionic content text
+      <IonContent className="ion-padding">
+        <IonList>
+          {arr.map(elem => (
+            <IonItemSliding key={elem.name}>
+              <IonItem>
+                <IonAvatar>
+                  <img
+                    src={`https://ionicframework.com/docs/demos/api/list/avatar-${elem.name.toLowerCase()}.png`}
+                    alt="logo"
+                  />
+                </IonAvatar>
+
+                <IonLabel className="ion-padding">
+                  <h2>{elem.name}</h2>
+                  <h3>{elem.desc}</h3>
+                </IonLabel>
+              </IonItem>
+              <IonItemOptions side="start">
+                <IonItemOption onClick={() => alert('You pressed the delete key!')}>Delete</IonItemOption>
+              </IonItemOptions>
+
+            </IonItemSliding>
+          ))}
+        </IonList>
       </IonContent>
     </IonPage>
   );
